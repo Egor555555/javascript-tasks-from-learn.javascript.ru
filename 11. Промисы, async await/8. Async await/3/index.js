8becd1d34c48d48e00e9d03e023d382614a5787b
Egor Zubov <113874188+Egor555555@users.noreply.github.com>
2025-10-07 08:27:10 +0500

    Add files via upload

---
+++ 11. Промисы, async await/8. Async await/3/index.js
@@ -0,0 +1,28 @@
+/* Есть «обычная» функция. Как можно внутри неё получить результат выполнения async–функции?
+
+async function wait() {
+  await new Promise(resolve => setTimeout(resolve, 1000));
+
+  return 10;
+}
+
+function f() {
+  // ...что здесь написать?
+  // чтобы вызвать wait() и дождаться результата "10" от async–функции
+  // не забывайте, здесь нельзя использовать "await"
+} */
+
+async function wait() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    return 10;
+}
+
+function f() {
+    // ...что здесь написать?
+    // чтобы вызвать wait() и дождаться результата "10" от async–функции
+    // не забывайте, здесь нельзя использовать "await"
+    return wait().then(alert);
+} 
+
+f();
